8f265d89cbe4de3c819550b85c10358fbf1f4931
Jan Kąkol <Jachu50012@gmail.com>
2014-04-06 08:28:19 +0200

    Add SpotData source.

--- src/main/java/pl/project/web/spot/SpotData.java
+++ src/main/java/pl/project/web/spot/SpotData.java
@@ -1,8 +1,50 @@
 package pl.project.web.spot;
 
 import pl.project.core.AbstractData;
+import pl.project.core.user.UserServiceBean;
+import pl.project.domain.ScoreBean;
 import pl.project.domain.SpotBean;
+import pl.project.domain.TeamBean;
+import pl.project.domain.UserBean;
+
+import java.util.List;
 
 public class SpotData extends AbstractData<SpotBean> {
 
+    private TeamBean teamBean;
+    private ScoreBean score;
+    private UserServiceBean userService;
+    private List<UserBean> users;
+
+    public TeamBean getTeamBean() {
+        return teamBean;
+    }
+
+    public void setTeamBean(TeamBean teamBean) {
+        this.teamBean = teamBean;
+    }
+
+    public ScoreBean getScore() {
+        return score;
+    }
+
+    public void setScore(ScoreBean score) {
+        this.score = score;
+    }
+
+    public UserServiceBean getUserService() {
+        return userService;
+    }
+
+    public void setUserService(UserServiceBean userService) {
+        this.userService = userService;
+    }
+
+    public List<UserBean> getUsers() {
+        return userService.getAll();
+    }
+
+    public void setUsers(List<UserBean> users) {
+        this.users = users;
+    }
 }
